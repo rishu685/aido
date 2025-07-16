ad6d15a444668f4540f21b62015fb2d094d9eda2
Rishabh Mishra <rishabh45628@gmail.com>
2025-07-16 22:15:16 +0530

    Updated support.tsx

--- src/pages/query.tsx
+++ src/pages/query.tsx
@@ -149,6 +149,8 @@ export default function Home() {
 }
 
 const Scene = () => {
+                }
+  )
   const viewport = useThree((state) => state.viewport);
   // Create a simple gradient background instead of loading texture
   return (
